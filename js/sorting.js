b024280cadffaf31ce1203663efd4ace86926a3c
abhishek Vaghela <abhishek.v@simformsolutions.com>
2023-03-27 18:18:52 +0530

    completed

---
+++ js/sorting.js
@@ -0,0 +1,133 @@
+// data sorting by dropdown
+let ascBtn = document.querySelector("#asc");
+let productFilter = document.querySelector("#productFilter");
+
+// by default acc
+productFilter.onclick = function () {
+  let value = this.value;
+  if (value == "id") {
+    userData.sort((a, b) => (a.id.toLowerCase() > b.id.toLowerCase() ? -1 : 1));
+    getDataFromLocal();
+  } else if (value == "productName") {
+    userData.sort((a, b) =>
+      a.productName.toLowerCase() > b.productName.toLowerCase() ? -1 : 1
+    );
+    getDataFromLocal();
+  } else if (value == "description") {
+    userData.sort((a, b) =>
+      a.description.toLowerCase() > b.description.toLowerCase() ? -1 : 1
+    );
+    getDataFromLocal();
+  } else if (value == "email") {
+    userData.sort((a, b) =>
+      a.email.toLowerCase() > b.email.toLowerCase() ? -1 : 1
+    );
+    getDataFromLocal();
+  } else if (value == "price") {
+    userData.sort((a, b) =>
+      Number(a.price.toLowerCase()) > Number(b.price.toLowerCase()) ? -1 : 1
+    );
+
+    getDataFromLocal();
+  } else if (value == "catogories") {
+    userData.sort((a, b) =>
+      a.catogories.toLowerCase() > b.catogories.toLowerCase() ? -1 : 1
+    );
+    getDataFromLocal();
+  } else if (value == "all") {
+    getDataFromLocal();
+  }
+  getDataFromLocal();
+};
+ascBtn.classList.remove("asc");
+ascBtn.classList.add("dsc");
+ascBtn.innerHTML = `<span class="asc-arrow"><i class="fa-solid fa-up-right"></i></span>&nbsp;Sort in Accending Order`;
+
+ascBtn.onclick = function () {
+  if (ascBtn.classList.contains("asc")) {
+    let value = productFilter.value;
+    if (value == "id") {
+      userData.sort((a, b) =>
+        a.id.toLowerCase() > b.id.toLowerCase() ? -1 : 1
+      );
+      getDataFromLocal();
+    } else if (value == "productName") {
+      userData.sort((a, b) =>
+        a.productName.toLowerCase() > b.productName.toLowerCase() ? -1 : 1
+      );
+      getDataFromLocal();
+    } else if (value == "description") {
+      userData.sort((a, b) =>
+        a.description.toLowerCase() > b.description.toLowerCase() ? -1 : 1
+      );
+      getDataFromLocal();
+    } else if (value == "email") {
+      userData.sort((a, b) =>
+        a.email.toLowerCase() > b.email.toLowerCase() ? -1 : 1
+      );
+      getDataFromLocal();
+    } else if (value == "price") {
+      userData.sort((a, b) =>
+        Number(a.price.toLowerCase()) > Number(b.price.toLowerCase()) ? -1 : 1
+      );
+
+      getDataFromLocal();
+    } else if (value == "catogories") {
+      userData.sort((a, b) =>
+        a.catogories.toLowerCase() > b.catogories.toLowerCase() ? -1 : 1
+      );
+      getDataFromLocal();
+    } else if (value == "all") {
+      getDataFromLocal();
+    }
+    getDataFromLocal();
+    ascBtn.classList.remove("asc");
+    ascBtn.classList.add("dsc");
+    ascBtn.innerHTML = `<span class="asc-arrow"><i class="fa-solid fa-up-right"></i></span>&nbsp;Sort in Accending Order`;
+  } else {
+    let value = productFilter.value;
+    if (value == "id") {
+      userData.sort((a, b) =>
+        a.id.toLowerCase() > b.id.toLowerCase() ? 1 : -1
+      );
+      getDataFromLocal();
+    } else if (value == "productName") {
+      userData.sort((a, b) =>
+        a.productName.toLowerCase() > b.productName.toLowerCase() ? 1 : -1
+      );
+      getDataFromLocal();
+    } else if (value == "description") {
+      userData.sort((a, b) =>
+        a.description.toLowerCase() > b.description.toLowerCase() ? 1 : -1
+      );
+      getDataFromLocal();
+    } else if (value == "email") {
+      userData.sort((a, b) =>
+        a.email.toLowerCase() > b.email.toLowerCase() ? 1 : -1
+      );
+      getDataFromLocal();
+    } else if (value == "price") {
+      userData.sort((a, b) => {
+        if (Number(a.price.toLowerCase()) > Number(b.price.toLowerCase())) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+
+      getDataFromLocal();
+    } else if (value == "catogories") {
+      userData.sort((a, b) =>
+        a.catogories.toLowerCase() > b.catogories.toLowerCase() ? 1 : -1
+      );
+      getDataFromLocal();
+    } else if (value == "all") {
+      getDataFromLocal();
+    }
+    console.log(userData);
+    getDataFromLocal();
+    ascBtn.classList.add("asc");
+    ascBtn.classList.remove("dsc");
+    ascBtn.innerHTML = `<span class="asc-arrow"><i class="fa-solid fa-down-right"></i></span>&nbsp;Sort in deccending Order`;
+  }
+};
